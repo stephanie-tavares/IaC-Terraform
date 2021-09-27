69b7dcbdfa71c12c87b6c535f21cd42cd8628fc7
stephanie-tavares <78547969+stephanie-tavares@users.noreply.github.com>
2021-09-27 17:05:30 -0300

    update variables.tf
    Adicionado vc_id, subnet_id e iam_instance_profile;

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,3 +1,11 @@
 variable "vpc_id" {
-   default = "vpc-seu-id"
+  default = "vpc-<seu-id>"
+}
+
+variable "subnet_id" {
+  default = "subnet-<seu-id>"
+}
+
+variable "iam_instance_profile" {
+   default = "<Sua IAM ROLE>"
 }
