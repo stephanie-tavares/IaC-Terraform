2ccdcddff72275e8d0f9c93b5398da0e053f6f84
stephanie-tavares <78547969+stephanie-tavares@users.noreply.github.com>
2021-09-27 17:03:43 -0300

    Atualização de código
    Melhorado a leitura da main.tf;
    Adicionado campo de user_data para script em Amazon Linux;

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,54 +1,39 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "EC2-Stephanie" {
   ami           = "ami-087c17d1fe0178315"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-<ID>"
-  security_groups = [ aws_security_group.ec2-rules.id]
-  user_data = file("${path.module}/instalador.sh") #Pra já subir a EC2 com alguns programas instalados, arquivo externo.
+  instance_type = "t2.small"
+  security_groups = [aws_security_group.ec2-rules.id]
+  iam_instance_profile = var.iam_instance_profile
+  key_name = ""
+  user_data = "${file("instalador.sh")}"
+  vpc_security_group_ids = [aws_security_group.ec2-rules.id]
+  subnet_id = var.subnet_id
 
   tags = {
-	Name = "Teste-EC2"
-	}
-}
-
-#Create VPC
-resource "aws_vpc" "terraform-vpc" {
-  cidr_block = "1.1.1.1/1"
-  enable_dns_hostnames = true 
-  
-  tags = {
-    Name = "VPC"
+    Name = "ec2-stephanie"
   }
 }
 
-output "aws_vpc_id" {
-  value = "${aws_vpc.terraform-vpc.id}"
-  
+output "DNS" {
+  value = aws_instance.EC2-Stephanie.public_dns
+}
+
+resource "aws_network_interface" "ec2" {
+  subnet_id = var.subnet_id
 }
 
 #Security Group
 resource "aws_security_group" "ec2-rules" {
-  name        = "regras-de-acesso-Entrada"
-  description = "regras-de-acesso-Entrada"
+  name        = "sg_stephanie"
+  description = "sg_stephanie"
   vpc_id      = var.vpc_id
+ 
+  tags = {
+    "Name" = "sg_stephanie"
+  }
 
-
-  ingress = [ 
+ ingress = [ 
         {
-        description = "Acesso-SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = false
-        }
-     ]
-
-  egress = [ 
-        {
-        description = "Allow-Access"
+        description = "Entrada"
         from_port = 0
         to_port = 0
         protocol = "-1"
@@ -57,6 +42,15 @@ resource "aws_security_group" "ec2-rules" {
         prefix_list_ids = []
         security_groups = []
         self = false
-        }
-     ]
+        }   
+       
+    ]
+
+  egress {
+    description = "Saida"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
