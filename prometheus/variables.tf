8e3238778af41984d551b59222f5a6c5cc6616a2
stephanie-tavares <78547969+stephanie-tavares@users.noreply.github.com>
2022-05-09 16:37:52 -0300

    Create variables.tf

---
+++ prometheus/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  default = "vpc-111111"
+}
+
+variable "subnet_id" {
+  default = "subnet-1111111"
+}
+
+variable "iam_instance_profile" {
+   default = "SSMManagedInstanceCore"
+}
