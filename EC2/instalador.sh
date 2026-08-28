177bc3c6766081cb0377bf9dcf2d146919e26a5d
Stephanie Tavares <78547969+stephanie-tavares@users.noreply.github.com>
2026-08-28 09:10:00 +0200

    Update instalador.sh to remove weak password warnings
    Removed comments about weak passwords and clarified installation steps.

--- EC2/instalador.sh
+++ EC2/instalador.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo adduser stephanie
-echo mudar@123 | sudo passwd --stdin stephanie #Não recomendado o uso de senhas fracas, adicionado apenas como exemplo
-echo mudar@123 | sudo passwd --stdin root #Não recomendado o uso de senhas fracas, adicionado apenas como exemplo
-sudo amazon-linux-extras install nginx1 #Padrão de instalação Amazon Linux, verificar o padrão que vc irá utilizar e atualizar
+echo mudar@123 | sudo passwd --stdin stephanie 
+echo mudar@123 | sudo passwd --stdin root 
+sudo amazon-linux-extras install nginx1 
 sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
 sudo echo "<p> Funciona! Stephanie Tavares</p>" >> /usr/share/nginx/html/index.html
